350e0c5d59d074faf2fa886748970b931eddcea8
sank0403 <58062842+sank0403@users.noreply.github.com>
2026-01-23 19:01:58 -0500

    Add files via upload

--- chnlnk.js
+++ chnlnk.js
@@ -6,7 +6,7 @@ if (!localStorage.clshowrules){
     localStorage.setItem("skipReloadOnce", "1");
 }
 
-const BUILD_VERSION = "2025.01.23.02";
+const BUILD_VERSION = "2025.01.23.03";
 
 if (localStorage.getItem("skipReloadOnce") === "1") {
     // Clear the flag and skip reload this one time
@@ -95,11 +95,7 @@ function postStatsToWhatsApp() {
 }
 
 function myFunctionOpenWA() {
-	/* window.open("https://wa.me/?text=urlencodedtext", "_blank"); */
-	/* var linktext = "Paste Stats and Share!"; */
-	/* var walink = "https://wa.me/?text=" + linktext; */
-	/* window.open(walink); */
-	window.open("https://api.whatsapp.com/", "_blank");
+	window.open("https://api.whatsapp.com/send", "_blank");
 }
 
 //Open Stats at end of game
